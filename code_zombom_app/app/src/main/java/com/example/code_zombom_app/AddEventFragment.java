eab995723d6123b82bb6fe18c1b6e95988147006
Tejwinder Johal <tejwinde@ualberta.ca>
2025-10-28 15:17:36 -0600

    Organizer_US.02.01.02_added_other_event_info

--- code_zombom_app/app/src/main/java/com/example/code_zombom_app/AddEventFragment.java
+++ code_zombom_app/app/src/main/java/com/example/code_zombom_app/AddEventFragment.java
@@ -13,17 +13,25 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.code_zombom_app.EventViewModel;
-import com.example.code_zombom_app.R;
+import java.util.ArrayList;
 
 public class AddEventFragment extends Fragment {
 
-    private EventViewModel eventViewModel;private EditText eventNameEditText;
+    private EventViewModel eventViewModel;
+    private EditText eventNameEditText;
+    private EditText maxPeopleEditText;
+    private EditText dateEditText;
+    private EditText deadlineEditText;
+    private EditText genreEditText;
+    private EditText locationEditText;
     private Button saveEventButton;
+    // Create list of events
+    ArrayList<ArrayList<String>> listOfEvents = new ArrayList<ArrayList<String>>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         // Scoped to the Activity, so it's shared between fragments.
         eventViewModel = new ViewModelProvider(requireActivity()).get(EventViewModel.class);
     }
@@ -36,22 +44,42 @@ public class AddEventFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //find all buttons TODO: add rest of buttons
         Button cancelButton = view.findViewById(R.id.cancelButton);
-        eventNameEditText = view.findViewById(R.id.editTextText2);
+
+        eventNameEditText = view.findViewById(R.id.editTextName);
+        maxPeopleEditText = view.findViewById(R.id.editTextMaxPeople);
+        dateEditText = view.findViewById(R.id.editTextDate);
+        deadlineEditText = view.findViewById(R.id.editTextDeadline);
+        genreEditText = view.findViewById(R.id.editTextGenre);
+        locationEditText = view.findViewById(R.id.editTextLocation);
+
+
         saveEventButton = view.findViewById(R.id.saveEventButton);
 
         cancelButton.setOnClickListener(v -> {
             NavHostFragment.findNavController(AddEventFragment.this).navigateUp();
         });
 
-        //TODO: make it so that all sections except for location must be filled out
         //TODO: add poster and QR code generation
         saveEventButton.setOnClickListener(v -> {
+            ArrayList<String> eventInfo = new ArrayList<>();
+            eventInfo.add(eventNameEditText.getText().toString());
+            eventInfo.add(maxPeopleEditText.getText().toString());
+            eventInfo.add(dateEditText.getText().toString());
+            eventInfo.add(deadlineEditText.getText().toString());
+            eventInfo.add(genreEditText.getText().toString());
+
             String eventName = eventNameEditText.getText().toString();
-            if (!eventName.isEmpty()) {
+            if (!eventName.isEmpty() && !maxPeopleEditText.getText().toString().isEmpty()
+                    && !dateEditText.getText().toString().isEmpty() && !deadlineEditText.getText().toString().isEmpty()
+                    && !genreEditText.getText().toString().isEmpty()) {
+                if (!locationEditText.getText().toString().isEmpty()) {
+                    eventInfo.add(locationEditText.getText().toString());
+                }
                 // Use the ViewModel to add the new event
-                eventViewModel.addEvent(eventName);
+                //eventViewModel.addEvent(eventName);
+                eventViewModel.addEvent(eventInfo.toString()); //just for testing
+                listOfEvents.add(eventInfo);
 
                 // Navigate back to the main fragment
                 NavHostFragment.findNavController(AddEventFragment.this).navigateUp();
