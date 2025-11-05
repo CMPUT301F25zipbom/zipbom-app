807d63c52ebae9664c342615830c087593b4b4a4
Dang Nguyen <dpn@ualberta.ca>
2025-11-04 18:26:35 -0700

    Add some safeguard for adding to lists in Event.java

--- code_zombom_app/app/src/main/java/com/example/code_zombom_app/Entrant.java
+++ code_zombom_app/app/src/main/java/com/example/code_zombom_app/Entrant.java
@@ -16,12 +16,12 @@ import java.util.List;
  * @see Event
  * @see Parcelable
  */
-public class Entrant extends Profile implements Parcelable {
+public class Entrant extends Profile {
     private boolean notificationsEnabled = true;
     private boolean lastNotificationReceived;
 
-    private ArrayList<Event> waitingEvents;
-    private ArrayList<EventParticipation> eventHistory;
+    private final ArrayList<Event> waitingEvents;
+    private final ArrayList<EventParticipation> eventHistory;
 
     /**
      * MUST always call this constructor. Initialises the collections so model methods remain safe
@@ -49,31 +49,31 @@ public class Entrant extends Profile implements Parcelable {
         this.type = "Entrant";
     }
 
-    protected Entrant(Parcel in) {
-        id = in.readString();
-        name = in.readString();
-        email = in.readString();
-        phone = in.readString();
-        deviceId = in.readString();
-        notificationsEnabled = in.readByte() != 0;
-        lastNotificationReceived = in.readByte() != 0;
-        waitingEvents = new ArrayList<>();
-        in.readTypedList(waitingEvents, Event.CREATOR);
-        eventHistory = new ArrayList<>();
-        in.readTypedList(eventHistory, EventParticipation.CREATOR);
-    }
+//    protected Entrant(Parcel in) {
+//        id = in.readString();
+//        name = in.readString();
+//        email = in.readString();
+//        phone = in.readString();
+//        deviceId = in.readString();
+//        notificationsEnabled = in.readByte() != 0;
+//        lastNotificationReceived = in.readByte() != 0;
+//        waitingEvents = new ArrayList<>();
+//        in.readTypedList(waitingEvents, Event.CREATOR);
+//        eventHistory = new ArrayList<>();
+//        in.readTypedList(eventHistory, EventParticipation.CREATOR);
+//    }
 
-    public static final Creator<Entrant> CREATOR = new Creator<Entrant>() {
-        @Override
-        public Entrant createFromParcel(Parcel in) {
-            return new Entrant(in);
-        }
-
-        @Override
-        public Entrant[] newArray(int size) {
-            return new Entrant[size];
-        }
-    };
+//    public static final Creator<Entrant> CREATOR = new Creator<Entrant>() {
+//        @Override
+//        public Entrant createFromParcel(Parcel in) {
+//            return new Entrant(in);
+//        }
+//
+//        @Override
+//        public Entrant[] newArray(int size) {
+//            return new Entrant[size];
+//        }
+//    };
 
     /**
      * @return whether this entrant opted in to organiser/admin notifications
@@ -179,103 +179,103 @@ public class Entrant extends Profile implements Parcelable {
 
     // endregion
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(id);
-        dest.writeString(name);
-        dest.writeString(email);
-        dest.writeString(phone);
-        dest.writeString(deviceId);
-        dest.writeByte((byte) (notificationsEnabled ? 1 : 0));
-        dest.writeByte((byte) (lastNotificationReceived ? 1 : 0));
-        dest.writeTypedList(waitingEvents);
-        dest.writeTypedList(eventHistory);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /**
-     * Value object describing an entrant's outcome for a given event.
-     */
-    public static class EventParticipation implements Parcelable {
-        public enum Outcome {
-            WAITLISTED,
-            SELECTED,
-            CONFIRMED,
-            DECLINED,
-            NOT_SELECTED
-        }
-
-        private final Event event;
-        private Outcome outcome;
-        private long decisionTimestamp;
-
-        public EventParticipation(Event event, Outcome outcome) {
-            this.event = event;
-            this.outcome = outcome;
-            this.decisionTimestamp = System.currentTimeMillis();
-        }
-
-        protected EventParticipation(Parcel in) {
-            event = in.readParcelable(Event.class.getClassLoader());
-            outcome = Outcome.valueOf(in.readString());
-            decisionTimestamp = in.readLong();
-        }
-
-        public static final Creator<EventParticipation> CREATOR = new Creator<EventParticipation>() {
-            @Override
-            public EventParticipation createFromParcel(Parcel in) {
-                return new EventParticipation(in);
-            }
-
-            @Override
-            public EventParticipation[] newArray(int size) {
-                return new EventParticipation[size];
-            }
-        };
-
-        /**
-         * @return event backing this participation record
-         */
-        public Event getEvent() {
-            return event;
-        }
-
-        /**
-         * @return current outcome/state for the entrant in this event
-         */
-        public Outcome getOutcome() {
-            return outcome;
-        }
-
-        /**
-         * Updates the outcome and resets the decision timestamp.
-         */
-        public void setOutcome(Outcome outcome) {
-            this.outcome = outcome;
-            this.decisionTimestamp = System.currentTimeMillis();
-        }
-
-        /**
-         * @return epoch milliseconds of the last outcome change
-         */
-        public long getDecisionTimestamp() {
-            return decisionTimestamp;
-        }
-
-        @Override
-        public void writeToParcel(Parcel dest, int flags) {
-            dest.writeParcelable(event, flags);
-            dest.writeString(outcome.name());
-            dest.writeLong(decisionTimestamp);
-        }
-
-        @Override
-        public int describeContents() {
-            return 0;
-        }
-    }
+//    @Override
+//    public void writeToParcel(Parcel dest, int flags) {
+//        dest.writeString(id);
+//        dest.writeString(name);
+//        dest.writeString(email);
+//        dest.writeString(phone);
+//        dest.writeString(deviceId);
+//        dest.writeByte((byte) (notificationsEnabled ? 1 : 0));
+//        dest.writeByte((byte) (lastNotificationReceived ? 1 : 0));
+//        dest.writeTypedList(waitingEvents);
+//        dest.writeTypedList(eventHistory);
+//    }
+//
+//    @Override
+//    public int describeContents() {
+//        return 0;
+//    }
+//
+//    /**
+//     * Value object describing an entrant's outcome for a given event.
+//     */
+//    public static class EventParticipation implements Parcelable {
+//        public enum Outcome {
+//            WAITLISTED,
+//            SELECTED,
+//            CONFIRMED,
+//            DECLINED,
+//            NOT_SELECTED
+//        }
+//
+//        private final Event event;
+//        private Outcome outcome;
+//        private long decisionTimestamp;
+//
+//        public EventParticipation(Event event, Outcome outcome) {
+//            this.event = event;
+//            this.outcome = outcome;
+//            this.decisionTimestamp = System.currentTimeMillis();
+//        }
+//
+//        protected EventParticipation(Parcel in) {
+//            event = in.readParcelable(Event.class.getClassLoader());
+//            outcome = Outcome.valueOf(in.readString());
+//            decisionTimestamp = in.readLong();
+//        }
+//
+//        public static final Creator<EventParticipation> CREATOR = new Creator<EventParticipation>() {
+//            @Override
+//            public EventParticipation createFromParcel(Parcel in) {
+//                return new EventParticipation(in);
+//            }
+//
+//            @Override
+//            public EventParticipation[] newArray(int size) {
+//                return new EventParticipation[size];
+//            }
+//        };
+//
+//        /**
+//         * @return event backing this participation record
+//         */
+//        public Event getEvent() {
+//            return event;
+//        }
+//
+//        /**
+//         * @return current outcome/state for the entrant in this event
+//         */
+//        public Outcome getOutcome() {
+//            return outcome;
+//        }
+//
+//        /**
+//         * Updates the outcome and resets the decision timestamp.
+//         */
+//        public void setOutcome(Outcome outcome) {
+//            this.outcome = outcome;
+//            this.decisionTimestamp = System.currentTimeMillis();
+//        }
+//
+//        /**
+//         * @return epoch milliseconds of the last outcome change
+//         */
+//        public long getDecisionTimestamp() {
+//            return decisionTimestamp;
+//        }
+//
+//        @Override
+//        public void writeToParcel(Parcel dest, int flags) {
+//            dest.writeParcelable(event, flags);
+//            dest.writeString(outcome.name());
+//            dest.writeLong(decisionTimestamp);
+//        }
+//
+//        @Override
+//        public int describeContents() {
+//            return 0;
+//        }
+//    }
 }
