f7dfcfcbfbeb0407e1e0217b8a20877e3dec6fc2
Dang Nguyen <dpn@ualberta.ca>
2025-11-05 18:00:38 -0700

    Minor changes

--- code_zombom_app/app/src/main/java/com/example/code_zombom_app/Entrant/EditProfile/EditProfileActivity.java
+++ code_zombom_app/app/src/main/java/com/example/code_zombom_app/Entrant/EditProfile/EditProfileActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.code_zombom_app.Helpers.MVC.GModel;
 import com.example.code_zombom_app.Helpers.MVC.TView;
 import com.example.code_zombom_app.Helpers.Models.LoadUploadProfileModel;
 import com.example.code_zombom_app.R;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class EditProfileActivity extends AppCompatActivity implements TView<LoadUploadProfileModel> {
     private String email;
@@ -17,10 +19,26 @@ public class EditProfileActivity extends AppCompatActivity implements TView<Load
 
         setContentView(R.layout.activity_entrant_profile);
         email = getIntent().getStringExtra("Email");
+
+        LoadUploadProfileModel model = new LoadUploadProfileModel(FirebaseFirestore.getInstance());
+        EditProfileController controller = new EditProfileController(model,
+                findViewById(R.id.imageButtonEntrantProfileName),
+                findViewById(R.id.imageButtonEntrantProfileEmail),
+                findViewById(R.id.imageButtonEntrantProfilePhone),
+                findViewById(R.id.textViewEntrantProfileName),
+                findViewById(R.id.textViewEntrantProfileEmail),
+                findViewById(R.id.textViewEntrantProfilePhone),
+                findViewById(R.id.buttonEntrantProfileBack),
+                findViewById(R.id.buttonEntrantProfileSave),
+                findViewById(R.id.buttonEntrantProfileDelete),
+                findViewById(R.id.toggleButtonEntrantProfileNotification),
+                findViewById(R.id.toggleButtonEntrantProfileLinkUnlinkDevice));
+        model.addView(this);
     }
 
     @Override
     public void update(LoadUploadProfileModel model) {
-
+        if (model.getState() == GModel.State.CLOSE)
+            finish();
     }
 }
